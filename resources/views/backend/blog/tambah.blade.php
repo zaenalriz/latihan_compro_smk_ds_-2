c08b1cda4d895866ca64cd6df1cef037517dbc4a
zaenalriz <muhammadzaenal855@gmail.com>
2024-08-28 11:51:00 +0700

    pertemuan 9

--- resources/views/backend/blog/tambah.blade.php
+++ resources/views/backend/blog/tambah.blade.php
@@ -10,7 +10,7 @@
                         aria-describedby="emailHelp" placeholder="Masukan judul">
                 </div>
                 <div class="form-group">
-                    <textarea name="" class="form-control" placeholder="masukan deskripsi" id="" cols="30" rows="3"></textarea>
+                    <textarea name="description" class="form-control" placeholder="masukan deskripsi" id="" cols="30" rows="3"></textarea>
                 </div>
                 <div class="form-group">
                     <input type="file" name="file" class="form-control form-control-user"
